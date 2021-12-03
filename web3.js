43c2008fbc73b4c215050ad9490ec1f8ba910456
Pedro <48381877+pierrot498@users.noreply.github.com>
2021-12-03 12:09:26 +0100

    Update web3.js

--- web3.js
+++ web3.js
@@ -52,7 +52,7 @@ $(function () {
         getAccount();
       }).on("click", ".nft-item", function(e){
         e.preventDefault();
-        NFT_QNT = parseInt($(this).data('nft'));
+        NFT_QNT = parseInt($('#nft').find(":selected").text(););
       });
       
       // Page Reload Handling
